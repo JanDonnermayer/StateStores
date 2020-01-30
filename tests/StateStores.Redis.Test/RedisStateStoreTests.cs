9867c08840276de1801ba442b623ff5f0286ab1a
JanDonnermayer <jan@donnermayer.de>
2020-01-30 16:18:14 +0100

    refactor format

--- tests/StateStores.Redis.Test/RedisStateStoreTests.cs
+++ tests/StateStores.Redis.Test/RedisStateStoreTests.cs
@@ -11,42 +11,42 @@ namespace StateStores.Redis.Test
     [TestFixture]
     public class RedisStateStoreTests
     {
-
         [SetUp]
         public void Setup() => 
             RedisStateStoreFactory.FlushAllDatabases();
 
-        private IStateStore GetStateStore() => 
+        private IStateStore GetStateStore() =>
             RedisStateStoreFactory.GetStateStore();
 
-
         [Test]
-        public Task TestBasicFunctionalityAsync() => 
+        public Task TestBasicFunctionalityAsync() =>
             GetStateStore()
-                .TestBasicFunctionalityAsync(key: Guid.NewGuid().ToString()); 
+                .TestBasicFunctionalityAsync(key: Guid.NewGuid().ToString()
+            );
 
         [Test]
-        public Task TestParallelFunctionalityAsync() => 
+        public Task TestParallelFunctionalityAsync() =>
             GetStateStore()
                 .TestParallelFunctionalityAsync(
                     activeChannelCount: 10,
                     stepBlockcount: 100,
-                    passiveChannelCount: 1000); 
+                    passiveChannelCount: 1000
+                ); 
 
         [Test]
-        public Task TestReactiveFunctionalityAsync() => 
+        public Task TestReactiveFunctionalityAsync() =>
             GetStateStore()
                 .ToChannel<int>("key1")
                 .TestReactiveFunctionalityAsync(
                     stepCount: 10,
-                    activeChannelCount: 5); 
+                    activeChannelCount: 5
+                );
 
         [Test]
-        public Task TestReplayFunctionalityAsync() => 
+        public Task TestReplayFunctionalityAsync() =>
             GetStateStore()
                 .ToChannel<string>("key2")
                 .TestReplayFunctionalityAsync();
-
     }
-    
+
 }
